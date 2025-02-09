4a8bbc57e058c4f45ad47b8f870d2dac888f53c4
KaryMonge <karinadurazno21@gmail.com>
2025-02-09 13:13:59 -0600

    Initial commit

--- obj/Debug/net6.0/Proyecto1_web.AssemblyInfo.cs
+++ obj/Debug/net6.0/Proyecto1_web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c80c074c75829278382d369902faf3b73366f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ce76c358c2973e0e29146264e2440fd4473516")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
